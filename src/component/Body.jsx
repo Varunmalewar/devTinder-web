9e22e2c6a87675076619055f6c4e88137a0f64dc
Varun Malewar <vmmalewar@gmail.com>
2025-07-10 09:39:02 +0530

    security checks

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../Utils/userSlice'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user)
+
+  const fetchUser = async () =>{
+    if(userData){
+      // If userData is already available, no need to fetch again
+      return;
+    }
+    try{const res = await axios.get(BASE_URL + "/profile/view",{
+      withCredentials : true} )
+       dispatch(addUser(res.data));
+    }
+
+     
+      catch(err){
+        if(err.status === 401){
+          navigate("/login");
+
+        }
+        // If the user is not logged in, redirect to login page
+        console.log(err);
+      }
+  }
+  useEffect(() =>{
+ 
+      // If userData is not available, fetch user data
+      fetchUser();
+
+    },[])
+
   return (
     <>
     <div>
